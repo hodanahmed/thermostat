d3af641e10e4c5967a025fa360c4318f46a3a153
Hodan <hodanahmed0000@gmail.com>
2020-03-26 18:05:33 +0000

    Created a limit of up function to 25 degrees

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -44,9 +44,23 @@ describe('Thermostat',function(){
     thermostat.switchPowerSavingModeOff();
     expect(thermostat.isPowerSavingMode()).toBe(false);
 });
-// testing for power saving mode to turn on - expect boolean true
-  it('can switch PSM on', function(){
-    thermostat.switchPowerSavingModeOn();
+
+  //New describe block for PSM on for testing to see if the require limit the PSM mode depending on temperature
+  //PSM turns on when temperature is < 25
+  describe('when power saving mode is on', function() {
+    //testing if power saving mode switches when its above 25
+    it('has a maximum temperature of 25 degrees', function() {
+
+      for (var i = 0; i < 6; i++) {
+      thermostat.up();
+      }
+    expect(thermostat.getCurrentTemperature()).toEqual(25);
+    });
+
+    // testing for power saving mode to turn on - expect boolean true
+    it('can switch PSM on', function(){
+      thermostat.switchPowerSavingModeOn();
     expect(thermostat.isPowerSavingMode()).toBe(true);
-});
+    });
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -7,19 +7,24 @@ class Thermostat {
   constructor() {
 //Adding minimum temperature property as a constant
     this.MINIMUM_TEMPERATURE = 10;
+    this.MAXIMUM_TEMPERATURE = 25;
     this.temperature = 20;
     this.powerSavingMode = true;
-  }
+  };
 
 // I created a new function called getCurrentTemperature which just
 // reads the this.temperature property at that moment.
   getCurrentTemperature () {
     return this.temperature
-  }
+  };
 //up function increments the temperature property
   up() {
+  //set guard condition so that the temperature has a limit of 25
+    if(this.isMaximumTemperature()){
+      return;
+    }
     this.temperature +=1
-  }
+  };
 //down function decrement the temperature property
   down() {
   //Created a guard condition so temperature property doesn't go down unless its > 10
@@ -27,23 +32,27 @@ class Thermostat {
       return;
     }
     this.temperature -=1
-  }
+  };
 //Created a switch on PSM mode
   switchPowerSavingModeOn() {
     this.powerSavingMode = true;
-  }
+  };
 //Created a switch off PSM mode
   switchPowerSavingModeOff() {
     this.powerSavingMode = false;
-  }
+  };
 
 //Created a MINIMUM_TEMPERATURE function so it can return a boolean to check if this has been set or not
 //the function starts with 'is', it's JS convention similar to ruby's ? as the function returns a boolean
   isMinimumTemperature() {
     return this.temperature === this.MINIMUM_TEMPERATURE;
-  }
+  };
 // Created a power saving mode
   isPowerSavingMode() {
     return this.powerSavingMode;
-  }
+  };
+//Created isMaximumTemperature function
+  isMaximumTemperature() {
+    return this.temperature === this.MAXIMUM_TEMPERATURE;
+  };
 };
