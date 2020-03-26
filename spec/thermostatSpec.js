f5d2ad4bb672691887f0c0c71fc90059ea9d69e4
Hodan <hodanahmed0000@gmail.com>
2020-03-26 16:56:54 +0000

    Added constant minimum temperature and guard conditioning for down function

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -5,28 +5,34 @@ describe('Thermostat',function(){
   beforeEach(function(){
   thermostat = new Thermostat();
   });
-
+//testing to see if it can make a new Thermostat object
   it('Can make a new instance of thermostat', function(){
     expect(thermostat).toBeInstanceOf(Thermostat);
   });
-
+//testing to see that default temp is 20
   it('starts at 20 degrees', function() {
     expect(thermostat.temperature).toEqual(20)
   });
-
+//testing to see if you can access this.temperature
   it('can get current temperature', function() {
       thermostat.temperature = 40;
     expect(thermostat.getCurrentTemperature()).toEqual(40)
   });
-
+//testing if the up function works
   it('the temperature can go up', function() {
       thermostat.up();
     expect(thermostat.getCurrentTemperature()).toEqual(21)
   });
-
+//testing if the down function works
   it('the temperature can go down', function() {
       thermostat.down();
     expect(thermostat.getCurrentTemperature()).toEqual(19)
   });
-
+//testing to see if it has a minimum of 10 degrees
+  it('has a minimum of 10 degrees', function() {
+    for (var i = 0; i < 11; i++) {
+      thermostat.down();
+    }
+    expect(thermostat.getCurrentTemperature()).toEqual(10);
+  });
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -5,6 +5,8 @@
 // which already set when you make a new instance of thermostat, for us it's 20 degrees.
 class Thermostat {
   constructor() {
+//Adding minimum temperature property as a constant
+    this.MINIMUM_TEMPERATURE = 10;
     this.temperature = 20;
   }
 
@@ -13,10 +15,17 @@ class Thermostat {
   getCurrentTemperature () {
     return this.temperature
   }
+//up function increments the temperature property
   up() {
     this.temperature +=1
   }
+//down function decrement the temperature property
   down() {
     this.temperature -=1
+  }
+//Created a MINIMUM_TEMPERATURE function so it can return a boolean to check if this has been set or not
+//the function starts with 'is', it's JS convention similar to ruby's ? as the function returns a boolean
+  isMinimumTemperature() {
+    return this.temperature === this.MINIMUM_TEMPERATURE;
   }
 };
