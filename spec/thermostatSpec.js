48feba8260f018d9a9043442b52ff3d594399071
Hodan <hodanahmed0000@gmail.com>
2020-03-26 15:23:42 +0000

    Created a passing test for the instance having set temp property of 20 degrees

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -3,15 +3,15 @@
 describe('Thermostat',function(){
   var thermostat;
   beforeEach(function(){
-    thermostat = new Thermostat();
+  thermostat = new Thermostat();
   });
 
   it('Can make a new instance of thermostat', function(){
-    expect(new Thermostat).toBeInstanceOf(Thermostat);
+    expect(thermostat).toBeInstanceOf(Thermostat);
   });
 
   it('starts at 20 degrees', function() {
-    expect(thermostat.temperature).toEqual(20);
+    expect(thermostat.temperature).toEqual(20)
   });
 
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -1,1 +1,10 @@
-class Thermostat{};
+
+// This class x {} is an object constructor function
+// It enables us to make instances of Thermostat - similar to def initialize
+// The 'this' gives the new instance a property (instance variables)
+// which already set when you make a new instance of thermostat.
+class Thermostat {
+  constructor() {
+    this.temperature = 20;
+  }
+};
