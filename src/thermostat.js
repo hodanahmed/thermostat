6f49b9c9d0ef1211282cb92473829ee65db1fcec
Hodan <hodanahmed0000@gmail.com>
2020-03-26 17:07:00 +0000

    Created a Power saving mode function with passing tests

--- src/thermostat.js
+++ src/thermostat.js
@@ -1,13 +1,14 @@
 
 // This class x {} is an object constructor function
 // It enables us to make instances of Thermostat - similar to def initialize
-// The 'this' gives the new instance a property (instance variables)
+// Getter methods - The 'this' gives the new instance a property (instance variables)
 // which already set when you make a new instance of thermostat, for us it's 20 degrees.
 class Thermostat {
   constructor() {
 //Adding minimum temperature property as a constant
     this.MINIMUM_TEMPERATURE = 10;
     this.temperature = 20;
+    this.powerSavingModeOn = true;
   }
 
 // I created a new function called getCurrentTemperature which just
@@ -31,5 +32,9 @@ class Thermostat {
 //the function starts with 'is', it's JS convention similar to ruby's ? as the function returns a boolean
   isMinimumTemperature() {
     return this.temperature === this.MINIMUM_TEMPERATURE;
+  }
+
+  isPowerSavingModeOn() {
+    return this.powerSavingModeOn === true;
   }
 };
