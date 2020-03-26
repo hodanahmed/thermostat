26a113f05e927afb5cd60c6728ffa4a819e6cb56
Hodan <hodanahmed0000@gmail.com>
2020-03-26 16:31:08 +0000

    Added functions for getting current temperature, up and down as well as passing tests

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -14,4 +14,19 @@ describe('Thermostat',function(){
     expect(thermostat.temperature).toEqual(20)
   });
 
+  it('can get current temperature', function() {
+      thermostat.temperature = 40;
+    expect(thermostat.getCurrentTemperature()).toEqual(40)
+  });
+
+  it('the temperature can go up', function() {
+      thermostat.up();
+    expect(thermostat.getCurrentTemperature()).toEqual(21)
+  });
+
+  it('the temperature can go down', function() {
+      thermostat.down();
+    expect(thermostat.getCurrentTemperature()).toEqual(19)
+  });
+
 });
--- src/thermostat.js
+++ src/thermostat.js
@@ -2,9 +2,21 @@
 // This class x {} is an object constructor function
 // It enables us to make instances of Thermostat - similar to def initialize
 // The 'this' gives the new instance a property (instance variables)
-// which already set when you make a new instance of thermostat.
+// which already set when you make a new instance of thermostat, for us it's 20 degrees.
 class Thermostat {
   constructor() {
     this.temperature = 20;
+  }
+
+// I created a new function called getCurrentTemperature which just
+// reads the this.temperature property at that moment.
+  getCurrentTemperature () {
+    return this.temperature
+  }
+  up() {
+    this.temperature +=1
+  }
+  down() {
+    this.temperature -=1
   }
 };
