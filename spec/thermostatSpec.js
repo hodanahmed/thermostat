b9527c9dba7b3d890e3f7286bb594d40e97ff159
Hodan <hodanahmed0000@gmail.com>
2020-03-26 14:49:48 +0000

    Passing instance test

--- spec/thermostatSpec.js
+++ spec/thermostatSpec.js
@@ -1,3 +1,5 @@
+'use strict';
+
 describe('Thermostat',function(){
   var thermostat;
   beforeEach(function(){
@@ -7,4 +9,9 @@ describe('Thermostat',function(){
   it('Can make a new instance of thermostat', function(){
     expect(new Thermostat).toBeInstanceOf(Thermostat);
   });
+
+  it('starts at 20 degrees', function() {
+    expect(thermostat.temperature).toEqual(20);
+  });
+
 });
